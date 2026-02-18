072ad4049d2f39b358473f776ef146fd815149b0
Ronald Huereca <ronald@mediaron.com>
2026-02-18 12:17:23 -0600

    Adding base role and accessibility.

--- src/core/withSpinnerControls.tsx
+++ src/core/withSpinnerControls.tsx
@@ -9,6 +9,17 @@ export type SpinnerProps = React.SVGProps<SVGSVGElement> & {
 export function withSpinnerControls(
   Comp: React.ComponentType<React.SVGProps<SVGSVGElement>>
 ) {
+  /**
+   * Wraps the spinner component with additional props and styles.
+   *
+   * @param {Object} props - The props for the spinner component.
+   * @param {number} props.size - The size of the spinner.
+   * @param {string} props.color - The color of the spinner.
+   * @param {number} props.speedMultiplier - The speed multiplier for the spinner.
+   * @param {string} props.className - The class name for the spinner.
+   * @param {Object} props.style - The style for the spinner.
+   * @return {React.ComponentType<React.SVGProps<SVGSVGElement>>} The wrapped spinner component.
+   */
   return function Spinner({
     size = 24,
     color,
@@ -25,6 +36,8 @@ export function withSpinnerControls(
         width={size}
         height={size}
         className={["dlx-spinner", className].filter(Boolean).join(" ")}
+		aria-label="Loading..."
+		role="img"
         style={{
           ...(color ? { color } : null),
           animationDuration: `${duration}s`,
