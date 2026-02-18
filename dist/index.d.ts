1fb650f90105d81d564a645afcefb1a6920f3403
Ronald Huereca <ronald@mediaron.com>
2026-02-18 14:25:27 -0600

    Adding demo packages.

---
+++ dist/index.d.ts
@@ -0,0 +1,40 @@
+import * as react from 'react';
+
+type SpinnerProps = react.SVGProps<SVGSVGElement> & {
+    size?: number | string;
+    color?: string;
+    speedMultiplier?: number;
+};
+
+/**
+ * AUTO-GENERATED FILE
+ * Do not edit manually.
+ */
+declare const ReactSpinner1: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner10: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner11: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner12: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner13: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner14: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner15: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner16: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner17: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner18: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner19: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner2: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner20: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner21: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner22: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner23: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner24: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner25: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner26: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner3: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner4: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner5: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner6: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner7: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner8: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+declare const ReactSpinner9: ({ size, color, speedMultiplier, className, style, ...rest }: SpinnerProps) => react.JSX.Element;
+
+export { ReactSpinner1, ReactSpinner10, ReactSpinner11, ReactSpinner12, ReactSpinner13, ReactSpinner14, ReactSpinner15, ReactSpinner16, ReactSpinner17, ReactSpinner18, ReactSpinner19, ReactSpinner2, ReactSpinner20, ReactSpinner21, ReactSpinner22, ReactSpinner23, ReactSpinner24, ReactSpinner25, ReactSpinner26, ReactSpinner3, ReactSpinner4, ReactSpinner5, ReactSpinner6, ReactSpinner7, ReactSpinner8, ReactSpinner9 };
